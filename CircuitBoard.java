be324f8e08e7b14c657e0faa450474e171855859
Alex Taylor <121984825+AlexETay7@users.noreply.github.com>
2023-12-06 18:22:56 -0700

    Add files via upload

---
+++ CircuitBoard.java
@@ -0,0 +1,241 @@
+import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * Represents a 2D circuit board as read from an input file.
+ * 
+ * @author mvail, Alex Taylor Fa-23 CS221
+ */
+public class CircuitBoard {
+	/** current contents of the board */
+	private char[][] board;
+	/** location of row,col for '1' */
+	private Point startingPoint;
+	/** location of row,col for '2' */
+	private Point endingPoint;
+
+	// constants you may find useful
+	private final int ROWS; // initialized in constructor
+	private final int COLS; // initialized in constructor
+	private final char OPEN = 'O'; // capital 'o'
+	private final char CLOSED = 'X';
+	private final char TRACE = 'T';
+	private final char START = '1';
+	private final char END = '2';
+	private final String ALLOWED_CHARS = "OXT12";
+
+	/**
+	 * Construct a CircuitBoard from a given board input file, where the first
+	 * line contains the number of rows and columns as ints and each subsequent
+	 * line is one row of characters representing the contents of that position.
+	 * Valid characters are as follows:
+	 * 'O' an open position
+	 * 'X' an occupied, unavailable position
+	 * '1' first of two components needing to be connected
+	 * '2' second of two components needing to be connected
+	 * 'T' is not expected in input files - represents part of the trace
+	 * connecting components 1 and 2 in the solution
+	 * 
+	 * @param filename - file containing a grid of characters
+	 *                 
+	 * @throws FileNotFoundException      if Scanner cannot read the file
+	 * @throws InvalidFileFormatException for any other format or content issue that
+	 *                                    prevents reading a valid input file
+	 */
+	public CircuitBoard(String filename) throws FileNotFoundException {
+
+		Scanner fileScan = new Scanner(new File(filename));
+		String firstLine = fileScan.nextLine();
+		int numRows = 0;
+		int numCols = 0;
+		int numOnes = 0;
+		int numTwos = 0;
+
+		// CHECKING FOR VALID FIRST LINE INPUT //
+		String[] parts = firstLine.split("\\s+"); // Split by whitespace
+		if (parts.length != 2) {
+			throw new InvalidFileFormatException("First line should contain two numeric values.");
+		}
+
+		try {
+			numRows = Integer.parseInt(parts[0]);
+			numCols = Integer.parseInt(parts[1]);
+
+		} catch (NumberFormatException e) {
+			throw new InvalidFileFormatException("First line should contain two numeric values.");
+		}
+
+		// SCAN IN BOARD VALUES INTO 2D ARRAY // 
+		board = new char[numRows][numCols];
+
+		try {
+			for (int i = 0; i < numRows; i++) {
+				for (int j = 0; j < numCols; j++) {
+					
+					board[i][j] = fileScan.next().charAt(0);
+
+					if (board[i][j] != 'O' && board[i][j] != 'X' && board[i][j] != '1' && board[i][j] != '2') {
+						throw new InvalidFileFormatException("This file contains invalid characters.");
+					} 
+					if (board[i][j] == '1') { // Set starting point
+						startingPoint = new Point(i, j);
+						numOnes++;
+					}
+					if (board[i][j] == '2') { // Set ending point
+						endingPoint = new Point(i, j);
+						numTwos++;
+					}
+					
+				}
+			}
+			} catch (NoSuchElementException e) {
+			throw new InvalidFileFormatException("This file has an unused space.");
+		}
+	
+		// CHECKING FOR # OF 1'S AND 2's //
+		if (numOnes != 1 || numTwos != 1) {
+			throw new InvalidFileFormatException("This file contains excess path characters.");
+		}
+
+		// CHECKING FOR VALID # OF ROWS //
+		int rowCount = 0;
+		fileScan.close();
+		Scanner secScanner = new Scanner(new File(filename));
+		secScanner.nextLine();
+		while (secScanner.hasNextLine()) {
+            // Make sure empty lines are not counted.
+                if (!secScanner.nextLine().isEmpty()) {
+                    rowCount++;
+                }
+            }
+        // If rows match make file valid.
+        if (rowCount != numRows) {
+            throw new InvalidFileFormatException("Rows do not match.");
+        }
+
+		// CHECKING FOR VALID # OF COLS //
+		Scanner thirdScan = new Scanner(new File(filename));
+		thirdScan.nextLine();
+		while (thirdScan.hasNextLine()) {
+			String nextLine = thirdScan.nextLine();
+			String[] numChar = nextLine.split("\\s+"); // Split by whitespace
+			if (numChar.length != numCols) {
+				throw new InvalidFileFormatException("Column do not match.");
+			}
+		}
+
+		ROWS = numRows;
+		COLS = numCols;
+
+		secScanner.close();
+
+	}
+
+	/**
+	 * Copy constructor - duplicates original board
+	 * 
+	 * @param original board to copy
+	 */
+	public CircuitBoard(CircuitBoard original) {
+		board = original.getBoard();
+		startingPoint = new Point(original.startingPoint);
+		endingPoint = new Point(original.endingPoint);
+		ROWS = original.numRows();
+		COLS = original.numCols();
+	}
+
+	/**
+	 * utility method for copy constructor
+	 * 
+	 * @return copy of board array
+	 */
+	private char[][] getBoard() {
+		char[][] copy = new char[board.length][board[0].length];
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				copy[row][col] = board[row][col];
+			}
+		}
+		return copy;
+	}
+
+	/**
+	 * Return the char at board position x,y
+	 * 
+	 * @param row row coordinate
+	 * @param col col coordinate
+	 * @return char at row, col
+	 */
+	public char charAt(int row, int col) {
+		return board[row][col];
+	}
+
+	/**
+	 * Return whether given board position is open
+	 * 
+	 * @param row
+	 * @param col
+	 * @return true if position at (row, col) is open
+	 */
+	public boolean isOpen(int row, int col) {
+		if (row < 0 || row >= board.length || col < 0 || col >= board[row].length) {
+			return false;
+		}
+		return board[row][col] == OPEN;
+	}
+
+	/**
+	 * Set given position to be a 'T'
+	 * 
+	 * @param row
+	 * @param col
+	 * @throws OccupiedPositionException if given position is not open
+	 */
+	public void makeTrace(int row, int col) {
+		if (isOpen(row, col)) {
+			board[row][col] = TRACE;
+		} else {
+			throw new OccupiedPositionException("row " + row + ", col " + col + "contains '" + board[row][col] + "'");
+		}
+	}
+
+	/** @return starting Point(row,col) */
+	public Point getStartingPoint() {
+		return new Point(startingPoint);
+	}
+
+	/** @return ending Point(row,col) */
+	public Point getEndingPoint() {
+		return new Point(endingPoint);
+	}
+
+	/** @return number of rows in this CircuitBoard */
+	public int numRows() {
+		return ROWS;
+	}
+
+	/** @return number of columns in this CircuitBoard */
+	public int numCols() {
+		return COLS;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				str.append(board[row][col] + " ");
+			}
+			str.append("\n");
+		}
+		return str.toString();
+	}
+
+}// class CircuitBoard
---
+++ CircuitTracer.java
@@ -0,0 +1,136 @@
+import java.awt.Point;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+/**
+ * Search for shortest paths between start and end points on a circuit board
+ * as read from an input file using either a stack or queue as the underlying
+ * search state storage structure and displaying output to the console or to
+ * a GUI according to options specified via command-line arguments.
+ * 
+ * @author mvail, Alex Taylor Fa-23 CS221
+ */
+public class CircuitTracer {
+
+	/** launch the program
+	 * @param args three required arguments:
+	 *  first arg: -s for stack or -q for queue
+	 *  second arg: -c for console output or -g for GUI output
+	 *  third arg: input file name 
+	 */
+	public static void main(String[] args) {
+		new CircuitTracer(args); //create this with args
+	}
+
+	/** Print instructions for running CircuitTracer from the command line. */
+	private void printUsage() {
+		System.out.println("Usage: java CircuitTracer [-s | -q] [-c | -g] <input_file>");
+		System.out.println("Options:");
+		System.out.println("  -s  Use a stack for storage");
+		System.out.println("  -q  Use a queue for storage");
+		System.out.println("  -c  Run program in console mode");
+		System.out.println("  -g  Run program in GUI mode (extra-credit--see below for details)");
+		System.out.println("  input_file  Name of the input file");
+		System.out.println();
+	}
+	
+	/** 
+	 * Set up the CircuitBoard and all other components based on command
+	 * line arguments.
+	 * 
+	 * @param args command line arguments passed through from main()
+	 */
+	public CircuitTracer(String[] args) {
+	
+		if (args.length != 3) {
+			printUsage();
+			return; //exit the constructor immediately
+		}
+
+		Storage<TraceState> stateStore = null;
+		ArrayList<TraceState> bestPaths = new ArrayList<>();
+		CircuitBoard theBoard = null;
+
+		// Check user console input
+		switch(args[0]) { //checking first input
+			case "-s":
+				stateStore = Storage.getStackInstance();
+				break;
+			case "-q":
+				stateStore = Storage.getQueueInstance();
+				break;
+			default:
+				printUsage();
+				return;
+			}
+		
+		switch(args[1]) { //checking second input
+			case "-c":
+				break;
+			case "-g":
+				System.out.println("Sorry, GUI currently not available.");
+				return;
+			default:
+				printUsage();
+				return;
+		}
+
+		// Exception Handling for constructor
+		try {
+			theBoard = new CircuitBoard(args[2]);
+		} catch (InvalidFileFormatException e) {
+			System.out.println(e.toString());
+			return;
+		} catch (FileNotFoundException e) {
+			System.out.println(e.toString());
+			return;
+		}
+
+		// Adding a new traceState object for each position adjacent to the initial position.
+
+		if (theBoard.isOpen(theBoard.getStartingPoint().x + 1, theBoard.getStartingPoint().y)) {
+			stateStore.store(new TraceState(theBoard, theBoard.getStartingPoint().x + 1, theBoard.getStartingPoint().y));
+		}
+		if (theBoard.isOpen(theBoard.getStartingPoint().x - 1, theBoard.getStartingPoint().y)) {
+			stateStore.store(new TraceState(theBoard, theBoard.getStartingPoint().x - 1, theBoard.getStartingPoint().y));
+		}
+		if (theBoard.isOpen(theBoard.getStartingPoint().x, theBoard.getStartingPoint().y + 1)) {
+			stateStore.store(new TraceState(theBoard, theBoard.getStartingPoint().x, theBoard.getStartingPoint().y + 1));
+		}
+		if (theBoard.isOpen(theBoard.getStartingPoint().x, theBoard.getStartingPoint().y - 1)) {
+			stateStore.store(new TraceState(theBoard, theBoard.getStartingPoint().x, theBoard.getStartingPoint().y - 1));
+		}
+		
+		TraceState initial;
+
+		while (!stateStore.isEmpty()) {
+			initial = stateStore.retrieve();
+			if (initial.isComplete()) { // If it is a solution, check if its better or the same as any current solutions we may have.
+				if (bestPaths.isEmpty() || initial.pathLength() == bestPaths.get(0).pathLength()) {
+					bestPaths.add(initial);
+				} else if (initial.pathLength() < bestPaths.get(0).pathLength()) {
+					bestPaths = new ArrayList<>();
+					bestPaths.add(initial);
+				}
+			} else { // If it is not a solution, continue to generate the next paths.
+				if (initial.isOpen(initial.getRow() + 1, initial.getCol())) {
+					stateStore.store(new TraceState(initial, initial.getRow() + 1, initial.getCol()));
+				}
+				if (initial.isOpen(initial.getRow() - 1, initial.getCol())) {
+					stateStore.store(new TraceState(initial, initial.getRow() - 1, initial.getCol()));
+				}
+				if (initial.isOpen(initial.getRow(), initial.getCol() + 1)) {
+					stateStore.store(new TraceState(initial, initial.getRow(), initial.getCol() + 1));
+				}
+				if (initial.isOpen(initial.getRow(), initial.getCol() - 1)) {
+					stateStore.store(new TraceState(initial, initial.getRow(), initial.getCol() - 1));
+				}
+			}
+		}
+		
+		for (TraceState path : bestPaths) {
+			System.out.println(path.toString());
+		}
+	}
+	
+} // class CircuitTracer
